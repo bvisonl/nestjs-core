8cc0cb591ca29bf476addacc19742c2335da24b0
Benjamín Visón <bvisonl@gmail.com>
2020-08-30 11:49:40 -0400

    Checking for null requests

--- rest/core.rest.filter-options.ts
+++ rest/core.rest.filter-options.ts
@@ -9,21 +9,21 @@ export class FilterOptions {
 
   constructor(request: Request) {
     // Set the page number
-    this.page = request.query['page'] ? Number.parseInt(request.query['page'].toString()) : 1;
+    this.page = request && request.query['page'] ? Number.parseInt(request.query['page'].toString()) : 1;
     this.page = this.page < 1 ? 1 : this.page;
 
     // Set the limit
-    this.limit = request.query['limit'] ? Number.parseInt(request.query['limit'].toString()) : 25;
+    this.limit = request && request.query['limit'] ? Number.parseInt(request.query['limit'].toString()) : 25;
     this.limit = this.limit < 1 ? 1 : 25;
 
     // Set the offset
     this.offset = (this.page - 1) * this.limit;
 
     // Set the search
-    this.search = request.query['search'] ? request.query['search'].toString() : '';
+    this.search = request && request.query['search'] ? request.query['search'].toString() : '';
 
     // Set the filters
-    this.filters = request.query['filters'];
+    this.filters = request && request.query['filters'];
   }
 
   setMaxResults(maxResults: number): FilterOptions {
